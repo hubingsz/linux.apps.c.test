94d3da8eb41be2ccdcba8c5edc7d82737371b82a
hubing <hubing@momenta.ai>
2019-12-23 18:20:33 +0800

    hubing finish hal arch

---
+++ hal/hal_arch.h
@@ -0,0 +1,37 @@
+ /*
+ *******************************************************************************
+ *
+ * Copyright (C) 2019 Momenta Incorporated - http://www.momenta.cn/
+ * ALL RIGHTS RESERVED
+ *
+ *******************************************************************************
+ */
+
+#ifndef __HAL_ARCH_H__
+#define __HAL_ARCH_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+/**
+ * hal_arch_init() - openvx sys app need to init when start.
+ * @desc: hal arch file handler
+ * @return: Success return 0, failure return <0.
+ * @ps: this is the first interface to call for Hal layer
+ */
+int hal_arch_init(int* desc);
+
+
+/**
+ * hal_arch_deinit() - openvx sys app need to deinit when start.
+ * @desc: hal arch file handler
+ * @return: Success return 0, failure return <0.
+ */
+int hal_arch_deinit(int desc);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // __HAL_ARCH_H__
